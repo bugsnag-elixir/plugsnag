636487b9f62ef64c3359ed4b5eb22376762c8258
Jared Norman <jared@freerunningtechnologies.com>
2014-08-02 12:23:41 -0700

    Add hex stuff
    Change-Id: Ia26d6482d4510fd1ced50959d353156168040465

--- mix.exs
+++ mix.exs
@@ -1,11 +1,21 @@
-defmodule Plugsnag.Mixfile do
+defmodule Bugsnag.Mixfile do
   use Mix.Project
 
   def project do
     [ app: :plugsnag,
       version: "0.0.1",
       elixir: "~> 0.14.3",
-      deps: deps ]
+      package: package,
+      description: """
+        A plug that catches errors and sends them to Bugsnag]
+      """,
+     deps: deps ]
+  end
+
+  def package do
+    [ contributors: [ "Jared Norman" ],
+      licenses: [ "MIT" ],
+      links: [ github: "https://github.com/jarednorman/plugsnag" ] ]
   end
 
   def application do
