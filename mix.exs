595f281814a0fc77795e5ffbdef8223d5164ff1a
Jared Norman <jared@freerunningtechnologies.com>
2014-08-04 13:36:12 -0700

    Bump to v0.0.1-dev
    Change-Id: I7e0901025ea998351a305ae02acc4d130b7e1024

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bugsnag.Mixfile do
 
   def project do
     [ app: :plugsnag,
-      version: "0.0.1",
+      version: "0.0.1-dev",
       elixir: "~> 0.14.3",
       package: package,
       description: """
