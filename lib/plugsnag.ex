50c86387252157fef65abb22a30543d5970c306c
Jared Norman <jared@freerunningtechnologies.com>
2014-08-04 17:35:56 -0700

    Implement basic reporting/reraising
    Change-Id: Iba8dfe07f1391211c2c63f995bea04c674091c9e

--- lib/plugsnag.ex
+++ lib/plugsnag.ex
@@ -1,2 +1,16 @@
 defmodule Plugsnag do
+  @behaviour Plug.Wrapper
+
+  def init(opts), do: opts
+
+  def wrap(conn, opts, func) do
+    try do
+      func.(conn)
+    rescue
+      exception ->
+        stacktrace = System.stacktrace
+        Bugsnag.report exception, stacktrace
+        reraise exception, stacktrace
+    end
+  end
 end
--- test/plugsnag_test.exs
+++ test/plugsnag_test.exs
@@ -1,7 +1,16 @@
 defmodule PlugsnagTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all do
+    Bugsnag.start
+    :ok
+  end
+
+  test "it reraises the exceptions" do
+    assert_raise ArithmeticError, "bad argument in arithmetic expression", fn ->
+      Plugsnag.wrap(nil, nil, fn(_conn) ->
+        1 + "test"
+      end)
+    end
   end
 end
