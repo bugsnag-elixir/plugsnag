63f9adba7a598b69045ed9445f57dac11fe30912
Jared Norman <jared@freerunningtechnologies.com>
2014-08-23 21:59:17 -0700

    Formatted mix.exs as per elixir project
    Change-Id: Ifdcd12f7a056612c1642acff056f83f846f1d49d

--- mix.exs
+++ mix.exs
@@ -2,29 +2,29 @@ defmodule Plugsnag.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :plugsnag,
-      version: "0.0.1-dev",
-      elixir: "~> 0.15.0",
-      package: package,
-      description: """
-        A plug that catches errors and sends them to Bugsnag
-      """,
-     deps: deps ]
+    [app: :plugsnag,
+     version: "0.0.1-dev",
+     elixir: "~> 0.15.0",
+     package: package,
+     description: """
+       A plug that catches errors and sends them to Bugsnag
+     """,
+     deps: deps]
   end
 
   def package do
-    [ contributors: [ "Jared Norman" ],
-      licenses: [ "MIT" ],
-      links: [ github: "https://github.com/jarednorman/plugsnag" ] ]
+    [contributors: ["Jared Norman"],
+     licenses: ["MIT"],
+     links: [github: "https://github.com/jarednorman/plugsnag"]]
   end
 
   def application do
-    [ applications: [] ]
+    [applications: []]
   end
 
   defp deps do
-    [ { :bugsnag, "~> 0.1.0-dev", github: "jarednorman/bugsnag-elixir", branch: "v0.1.0-dev" },
-      { :cowboy, "~> 1.0.0" },
-      { :plug, "~> 0.5.1" } ]
+    [{:bugsnag, "~> 0.1.0-dev", github: "jarednorman/bugsnag-elixir", branch: "v0.1.0-dev"},
+     {:cowboy, "~> 1.0.0"},
+     {:plug, "~> 0.5.1"}]
   end
 end
