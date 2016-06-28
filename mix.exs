1a24fec2ccd2a4f66fa0e703d670329afa51cd1c
Andrew Harvey <andrew@mootpointer.com>
2016-06-29 08:45:58 +1000

    Version 1.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Plugsnag.Mixfile do
 
   def project do
     [app: :plugsnag,
-     version: "1.1.0",
+     version: "1.2.0",
      elixir: "~> 1.0",
      package: package,
      description: """
