5596ded6c70e14b6b0ec774a3d24245c15b00667
Erik Nilsen <enilsen16@live.com>
2017-07-11 04:48:56 -0700

    add custom grouping hash to struct

--- lib/plugsnag/error_report.ex
+++ lib/plugsnag/error_report.ex
@@ -6,9 +6,10 @@ defmodule Plugsnag.ErrorReport do
   @type t ::  %__MODULE__{
     severity: String.t | nil,
     context: String.t | nil,
+    custom_grouping_hash: String.t | nil,
     metadata: map() | nil,
     user: map() | nil
   }
 
-  defstruct [:severity, :context, :metadata, :user]
+  defstruct [:severity, :context, :custom_grouping_hash, :metadata, :user]
 end
