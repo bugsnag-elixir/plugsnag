82ec208ac71605c6e10cf35f62a09f8f4bba6f5a
Jared Norman <jared@super.gd>
2019-09-06 14:59:43 -0700

    Bump ex_doc
    Fixes error while publishing.

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Plugsnag.Mixfile do
   defp deps do
     [{:bugsnag, "~> 1.3 or ~> 2.0"},
      {:plug, "~> 1.0"},
-     {:ex_doc, ">= 0.0.0", only: :dev},
+     {:ex_doc, "~> 0.19", only: :dev},
      {:dialyxir, "~> 0.3.5", only: [:dev]}
     ]
   end
