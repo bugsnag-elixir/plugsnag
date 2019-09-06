e44ddbde0cbb7e88081b693cd0c937e5b626a40e
Sergei Maximov <s.b.maximov@gmail.com>
2019-09-06 14:48:04 +0300

    Relax requirements for Bugsnag to allow 2.x versions

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Plugsnag.Mixfile do
   end
 
   defp deps do
-    [{:bugsnag, "~> 1.3"},
+    [{:bugsnag, "~> 1.3 or ~> 2.0"},
      {:plug, "~> 1.0"},
      {:ex_doc, ">= 0.0.0", only: :dev},
      {:dialyxir, "~> 0.3.5", only: [:dev]}
