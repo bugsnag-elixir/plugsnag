c04b0c623238fd476ad865e770929a4fcc8a4d37
nirev <guilherme@nirev.org>
2020-07-06 18:26:14 -0300

    only handle 5xx exceptions

--- lib/plugsnag.ex
+++ lib/plugsnag.ex
@@ -2,41 +2,37 @@ defmodule Plugsnag do
   defmacro __using__(options \\ []) do
     quote location: :keep do
       use Plug.ErrorHandler
-      import Plugsnag
 
-      if :code.is_loaded(Phoenix) do
-        defp handle_errors(_conn, %{reason: %Phoenix.Router.NoRouteError{}}) do
-          nil
+      defp handle_errors(conn, %{reason: exception, kind: :error} = assigns) do
+        # Only handle exceptions that get rendered as an HTTP 5xx status
+        if Plug.Exception.status(exception) >= 500 do
+          do_handle_errors(conn, assigns)
         end
       end
 
-      if :code.is_loaded(Ecto) do
-        defp handle_errors(conn, %{reason: %Ecto.NoResultsError{}}) do
-          nil
-        end
+      defp handle_errors(conn, %{reason: _exception} = assigns) do
+        do_handle_errors(conn, assigns)
       end
 
-      defp handle_errors(conn, %{reason: exception}) do
-        error_report_builder = unquote(
-          Keyword.get(
-            options,
-            :error_report_builder,
-            Plugsnag.BasicErrorReportBuilder
+      defp do_handle_errors(conn, %{reason: exception}) do
+        error_report_builder =
+          unquote(
+            Keyword.get(
+              options,
+              :error_report_builder,
+              Plugsnag.BasicErrorReportBuilder
+            )
           )
-        )
 
         options =
           %Plugsnag.ErrorReport{}
           |> error_report_builder.build_error_report(conn)
-          |> Map.from_struct
-          |> Keyword.new
+          |> Map.from_struct()
+          |> Keyword.new()
 
-        apply(reporter(), :report, [exception | [options]])
+        reporter = Application.get_env(:plugsnag, :reporter, Bugsnag)
+        apply(reporter, :report, [exception | [options]])
       end
     end
-  end
-
-  def reporter do
-    Application.get_env(:plugsnag, :reporter, Bugsnag)
   end
 end
