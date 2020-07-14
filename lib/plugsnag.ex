a81e1f8179edc43f7c61515be369abb7eaa6c195
nirev <guilherme@nirev.org>
2020-07-14 11:40:13 -0300

    expose Plugsnag.handle_errors

--- lib/plugsnag.ex
+++ lib/plugsnag.ex
@@ -3,20 +3,26 @@ defmodule Plugsnag do
     quote location: :keep do
       use Plug.ErrorHandler
 
-      defp handle_errors(conn, %{reason: exception, kind: :error} = assigns) do
-        # Only handle exceptions that get rendered as an HTTP 5xx status
-        if Plug.Exception.status(exception) >= 500 do
-          Plugsnag.handle_errors(conn, assigns, unquote(options))
-        end
-      end
-
-      defp handle_errors(conn, %{reason: _exception} = assigns) do
+      defp handle_errors(conn, assigns) do
         Plugsnag.handle_errors(conn, assigns, unquote(options))
       end
     end
   end
 
-  def handle_errors(conn, %{reason: exception}, opts \\ []) do
+  def handle_errors(conn, assigns, opts \\ [])
+
+  def handle_errors(conn, %{reason: exception, kind: :error} = assigns, opts) do
+    # Only handle exceptions that get rendered as an HTTP 5xx status
+    if Plug.Exception.status(exception) >= 500 do
+      report_error(conn, assigns, opts)
+    end
+  end
+
+  def handle_errors(conn, %{reason: _exception} = assigns, opts) do
+    report_error(conn, assigns, opts)
+  end
+
+  defp report_error(conn, %{reason: exception}, opts) do
     error_report_builder =
       Keyword.get(opts, :error_report_builder, Plugsnag.BasicErrorReportBuilder)
 
