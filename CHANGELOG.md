c1809aeee0646235f68f39a89e356f1f02627318
nirev <guilherme@nirev.org>
2020-07-19 19:38:15 -0300

    bump to 1.5.0

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## v1.5.0
+
+### Added
+- expose public function handle_errors/3 to allow composition
+
+### Changed
+- only report exceptions mapping to 5xx errors
+- support only for elixir 1.6+
--- mix.exs
+++ mix.exs
@@ -2,23 +2,24 @@ defmodule Plugsnag.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :plugsnag,
-     version: "1.4.0",
-     elixir: "~> 1.3",
-     package: package(),
-     description: """
-       Bugsnag reporter for Elixir's Plug
-     """,
-     deps: deps(),
-     dialyzer: [plt_add_deps: :project]
-   ]
+    [
+      app: :plugsnag,
+      version: "1.5.0",
+      elixir: "~> 1.6",
+      package: package(),
+      description: "Bugsnag reporter for Elixir's Plug",
+      deps: deps(),
+      dialyzer: [plt_add_deps: :project]
+    ]
   end
 
   def package do
-    [contributors: ["Jared Norman", "Andrew Harvey"],
-     maintainers: ["Andrew Harvey"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/bugsnag-elixir/plugsnag"}]
+    [
+      contributors: ["Jared Norman", "Andrew Harvey", "Guilherme de Maio"],
+      maintainers: ["Andrew Harvey", "Guilherme de Maio"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/bugsnag-elixir/plugsnag"}
+    ]
   end
 
   def application do
@@ -26,10 +27,11 @@ defmodule Plugsnag.Mixfile do
   end
 
   defp deps do
-    [{:bugsnag, "~> 1.3 or ~> 2.0"},
-     {:plug, "~> 1.0"},
-     {:ex_doc, "~> 0.19", only: :dev},
-     {:dialyxir, "~> 1.0", only: [:dev]}
+    [
+      {:bugsnag, "~> 1.3 or ~> 2.0"},
+      {:plug, "~> 1.0"},
+      {:ex_doc, "~> 0.19", only: :dev},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
