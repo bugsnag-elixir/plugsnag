a782f6a39e1c6665ca9f3d78271c6a3512ef7510
Kian-Meng, Ang <kianmeng@cpan.org>
2021-12-30 21:08:27 +0800

    Misc doc changes
    Besides other documentation changes, this commit includes all relevant
    documents to the generated HTML doc for HexDocs.pm and leverages on
    latest features of ExDoc.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,17 +1,22 @@
 # Changelog
 
-## v1.6.1
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## v1.6.1 (2021-03-16)
 
 ### Fixed
 - pass along stacktrace from Plug.ErrorHandler to Bugsnag [#44](https://github.com/bugsnag-elixir/plugsnag/pull/44)
 
-## v1.6.0
+## v1.6.0 (2020-11-25)
 
 ### Changed
 - update dependency requirements to support bugsnag v3.0.0
   please check the [CHANGELOG](https://github.com/bugsnag-elixir/bugsnag-elixir/blob/master/CHANGELOG.md)
 
-## v1.5.0
+## v1.5.0 (2020-07-25)
 
 ### Added
 - expose public function handle_errors/3 to allow composition
--- mix.exs
+++ mix.exs
@@ -1,24 +1,32 @@
 defmodule Plugsnag.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/bugsnag-elixir/plugsnag"
+  @version "1.6.1"
+
   def project do
     [
       app: :plugsnag,
-      version: "1.6.1",
+      version: @version,
       elixir: "~> 1.8",
       package: package(),
-      description: "Bugsnag reporter for Elixir's Plug",
       deps: deps(),
-      dialyzer: [plt_add_deps: :project]
+      docs: docs(),
+      dialyzer: [plt_add_deps: :project],
+      preferred_cli_env: [docs: :docs, "hex.publish": :docs]
     ]
   end
 
   def package do
     [
+      description: "Bugsnag reporter for Elixir's Plug",
       contributors: ["Jared Norman", "Andrew Harvey", "Guilherme de Maio"],
       maintainers: ["Andrew Harvey", "Guilherme de Maio"],
       licenses: ["MIT"],
-      links: %{github: "https://github.com/bugsnag-elixir/plugsnag"}
+      links: %{
+        Changelog: "https://hexdocs.pm/plugsnag/changelog.html",
+        GitHub: @source_url
+      }
     ]
   end
 
@@ -26,8 +34,22 @@ defmodule Plugsnag.Mixfile do
     [
       {:bugsnag, "~> 1.3 or ~> 2.0 or ~> 3.0"},
       {:plug, "~> 1.0"},
-      {:ex_doc, "~> 0.19", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :docs, runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "CHANGELOG.md": [],
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"]
     ]
   end
 end
